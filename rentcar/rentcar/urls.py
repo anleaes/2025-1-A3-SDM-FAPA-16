56ec6541d710420e79422844769772025e43c8f9
IkkiMachado <hique.reis@hotmail.com>
2025-06-25 11:51:40 -0300

    Configura URLs para Veiculo

--- rentcar/rentcar/urls.py
+++ rentcar/rentcar/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('apps.clientes.urls')),  # Nova linha
+    path('api/', include('apps.clientes.urls')),
+    path('api/', include('apps.veiculos.urls')),
 ]
