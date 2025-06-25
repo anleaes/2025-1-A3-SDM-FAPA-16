02ac3d695d30c63a6746ee8a1aabad10f4d838e8
IkkiMachado <hique.reis@hotmail.com>
2025-06-25 14:59:14 -0300

    update models

--- rentcar/apps/veiculos/models.py
+++ rentcar/apps/veiculos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from apps.categorias.models import Categoria
 
-# Create your models here.
+class Veiculo(models.Model):
+    placa = models.CharField(max_length=10, unique=True)
+    valor_diaria = models.DecimalField(max_digits=10, decimal_places=2)
+    disponivel = models.BooleanField(default=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
+    
+    class Meta:
+        app_label = 'veiculos'
+    
+    def __str__(self):
+        return f"{self.placa} ({'Disponível' if self.disponivel else 'Indisponível'})"
