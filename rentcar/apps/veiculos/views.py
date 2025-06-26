4b5de521cacab65a8607e0dcf16f63855291918b
IkkiMachado <hique.reis@hotmail.com>
2025-06-26 12:06:01 -0300

    atualização views veiculos

--- rentcar/apps/veiculos/views.py
+++ rentcar/apps/veiculos/views.py
@@ -5,3 +5,10 @@ from .serializers import VeiculoSerializer
 class VeiculoViewSet(viewsets.ModelViewSet):
     queryset = Veiculo.objects.all()
     serializer_class = VeiculoSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        disponivel = self.request.query_params.get('disponivel')
+        if disponivel is not None:
+            queryset = queryset.filter(disponivel=disponivel.lower() == 'true')
+        return queryset
